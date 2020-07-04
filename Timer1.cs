20fdb699916ccd718a8b7b91b3308d72f1dac8d5
HamideAkkoca <64319801+HamideAkkoca@users.noreply.github.com>
2020-07-04 09:38:51 +0300

    Add files via upload

---
+++ Timer1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer2 : MonoBehaviour
+{
+    public int timeleft = 50;
+    public Text countdownText;
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+    }
+    void Update()
+    {
+        countdownText.text = ("Kalan Zaman = " + timeleft);
+
+        if (timeleft <= 0)
+        {
+            StopCoroutine("LoseTime");
+            countdownText.text = "Times Up!";
+        }
+    }
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeleft--;
+        }
+    }
+}
+
